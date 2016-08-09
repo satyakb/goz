7496118424bb67f8469e00f839656c72f3759c0a
Satya Bodduluri <skbodduluri@gmail.com>
2016-08-08 23:22:47 -0700

    moved to kafka and redis

--- client.go
+++ client.go
@@ -1,59 +1,59 @@
 package main
 
 import (
-    "bytes"
-    "fmt"
-    "io"
-    "io/ioutil"
-    "mime/multipart"
-    "net/http"
-    "os"
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"os"
 )
 
 func postFile(filename string, targetUrl string) error {
-    bodyBuf := &bytes.Buffer{}
-    bodyWriter := multipart.NewWriter(bodyBuf)
+	bodyBuf := &bytes.Buffer{}
+	bodyWriter := multipart.NewWriter(bodyBuf)
 
-    // this step is very important
-    fileWriter, err := bodyWriter.CreateFormFile("uploadfile", filename)
-    if err != nil {
-        fmt.Println("error writing to buffer")
-        return err
-    }
+	// this step is very important
+	fileWriter, err := bodyWriter.CreateFormFile("uploadfile", filename)
+	if err != nil {
+		fmt.Println("error writing to buffer")
+		return err
+	}
 
-    // open file handle
-    fh, err := os.Open(filename)
-    if err != nil {
-        fmt.Println("error opening file")
-        return err
-    }
+	// open file handle
+	fh, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("error opening file")
+		return err
+	}
 
-    //iocopy
-    _, err = io.Copy(fileWriter, fh)
-    if err != nil {
-        return err
-    }
+	//iocopy
+	_, err = io.Copy(fileWriter, fh)
+	if err != nil {
+		return err
+	}
 
-    contentType := bodyWriter.FormDataContentType()
-    bodyWriter.Close()
+	contentType := bodyWriter.FormDataContentType()
+	bodyWriter.Close()
 
-    resp, err := http.Post(targetUrl, contentType, bodyBuf)
-    if err != nil {
-        return err
-    }
-    defer resp.Body.Close()
-    resp_body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return err
-    }
-    fmt.Println(resp.Status)
-    fmt.Println(string(resp_body))
-    return nil
+	resp, err := http.Post(targetUrl, contentType, bodyBuf)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	resp_body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Println(resp.Status)
+	fmt.Println(string(resp_body))
+	return nil
 }
 
 // sample usage
 func main() {
-    target_url := "http://localhost:8000/share"
-    filename := "./song.mp3"
-    postFile(filename, target_url)
+	target_url := "http://localhost:8000/share"
+	filename := os.Args[1]
+	postFile(filename, target_url)
 }
--- server.go
+++ server.go
@@ -1,109 +1,164 @@
 package main
 
 import (
-  "fmt"
-  "io"
-  "net/http"
-  // "os"
-  "sync"
-  "time"
+	"github.com/Shopify/sarama"
+	"github.com/garyburd/redigo/redis"
+
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
+const BufferLen = 1024
+const KafkaTopic = "audio"
+const RedisKey = "offset"
+
 type flushWriter struct {
-  f http.Flusher
-  w io.Writer
+	f http.Flusher
+	w io.Writer
 }
 
 func (fw *flushWriter) Write(p []byte) (n int, err error) {
-  n, err = fw.w.Write(p)
-  if fw.f != nil {
-    fw.f.Flush()
-  }
-  return
+	n, err = fw.w.Write(p)
+	if err != nil {
+		return n, err
+	}
+	if fw.f != nil {
+		fw.f.Flush()
+	}
+	return n, err
 }
 
-var c = make(chan []byte, 1024)
-var listeners = map[int]flushWriter{}
-var counter = 0
-var mutex = &sync.Mutex{}
-
+var producer sarama.SyncProducer
+var redisPool *redis.Pool
 
 func hello(w http.ResponseWriter, r *http.Request) {
-  io.WriteString(w, "Hello world!")
+	io.WriteString(w, "Hello world!")
 }
 
 func shareHandler(w http.ResponseWriter, r *http.Request) {
-  reader, err := r.MultipartReader()
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
+	reader, err := r.MultipartReader()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-  // Goroutine that sprays audio to all listeners
-  go spray()
+	for {
+		part, err_part := reader.NextPart()
+		if err_part == io.EOF {
+			break
+		}
+		fmt.Println(part)
+		if part.FormName() == "uploadfile" {
+			for {
+				buf := make([]byte, BufferLen)
+				n, err := part.Read(buf)
+				if err == io.EOF {
+					break
+				}
 
-  for {
-    part, err_part := reader.NextPart()
-    if err_part == io.EOF {
-      break
-    }
-    fmt.Println(part)
-    if part.FormName() == "uploadfile" {
-      buf := make([]byte, 1024)
-      for {
-        n, err := part.Read(buf)
-        if err == io.EOF {
-          break
-        }
-        // Have to copy to a new byte array for SOME UNKNOWN REASON
-        final_buf := append([]byte{}, buf[:n]...)
-        c <- final_buf
-      }
-    }
-  }
+				// Write to Kafka
+				msg := &sarama.ProducerMessage{Topic: KafkaTopic, Value: sarama.ByteEncoder(buf[:n])}
+				_, _, err = producer.SendMessage(msg)
+				if err != nil {
+					log.Printf("FAILED to send message: %s\n", err)
+				}
+			}
+		}
+	}
 }
 
 func listenHandler(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-type", "audio/mpeg")
-  w.Header().Set("Transfer-Encoding", "chunked")
-  w.WriteHeader(200)
+	w.Header().Set("Content-type", "audio/mpeg")
+	w.Header().Set("Transfer-Encoding", "chunked")
+	w.WriteHeader(200)
 
-  fw := flushWriter{w: w}
-  if f, ok := w.(http.Flusher); ok {
-    fw.f = f
-  }
+	fw := flushWriter{w: w}
+	if f, ok := w.(http.Flusher); ok {
+		fw.f = f
+	}
 
-  mutex.Lock()
-  listeners[counter] = fw
-  counter++
-  fmt.Println(listeners)
-  mutex.Unlock()
+	spray(fw)
 
-  // Keep connection alive
-  for {
-    time.Sleep(time.Second * 10)
-  }
 }
 
-func spray() {
-  for {
-    time.Sleep(time.Millisecond)
-    if len(listeners) == 0 {
-      continue
-    }
+func spray(fw flushWriter) {
+	consumer, err := sarama.NewConsumer([]string{"localhost:9092"}, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := consumer.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
 
-    audio := <- c
-    mutex.Lock()
-    for _, fw := range listeners {
-      fw.Write(audio)
-    }
-    mutex.Unlock()
-  }
+	conn := redisPool.Get()
+	defer conn.Close()
+
+	offset, err := redis.Int64(conn.Do("GET", RedisKey))
+	// Will need these lines later when client is made realtime
+	// if err != nil {
+	// 	offset = sarama.OffsetNewest
+	// }
+
+	partitionConsumer, err := consumer.ConsumePartition(KafkaTopic, 0, offset)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := partitionConsumer.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	for {
+		time.Sleep(time.Millisecond * 20)
+
+		audio := []byte{}
+		select {
+		case msg := <-partitionConsumer.Messages():
+			audio = msg.Value
+
+			msgOffset := msg.Offset
+			offset, _ := redis.Int64(conn.Do("GET", RedisKey))
+			if msgOffset > offset {
+				conn.Do("SET", RedisKey, msgOffset)
+			}
+		default:
+			fmt.Println("DONE")
+		}
+
+		_, err := fw.Write(audio)
+		if err != nil {
+			fmt.Println("error writing to response")
+			break
+		}
+	}
 }
 
 func main() {
-  http.HandleFunc("/", hello)
-  http.HandleFunc("/share", shareHandler)
-  http.HandleFunc("/listen", listenHandler)
-  http.ListenAndServe(":8000", nil)
+	var err error
+	producer, err = sarama.NewSyncProducer([]string{"localhost:9092"}, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer func() {
+		if err := producer.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	redisPool = redis.NewPool(func() (redis.Conn, error) {
+		return redis.Dial("tcp", ":6379")
+	}, 8)
+
+	http.HandleFunc("/", hello)
+	http.HandleFunc("/share", shareHandler)
+	http.HandleFunc("/listen", listenHandler)
+	http.ListenAndServe(":"+os.Args[1], nil)
 }
